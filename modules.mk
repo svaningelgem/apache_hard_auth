511b2a5e09b556b1ead898fa74e850b6bf057b3b
atanas <steven@vaningelgem.be>
2009-08-31 13:47:19 +0000

    Initial Version

---
+++ modules.mk
@@ -0,0 +1,4 @@
+mod_auth.la: mod_auth.slo mod_auth_aux.slo
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_auth.lo mod_auth_aux.lo -lsqlapi
+DISTCLEAN_TARGETS = modules.mk
+shared =  mod_auth.la
